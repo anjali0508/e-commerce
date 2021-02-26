c7715164a54ae71f9c1306baa7bb467187031b5a
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-26 13:18:04 +0530

    feat(error-handline): add error handline

--- src/handlers/category.handler.js
+++ src/handlers/category.handler.js
@@ -1,17 +1,27 @@
 const categoryService = require('../services/category.service');
 
 const createCategory = async (req, res) => {
-  const { category } = req.params;
-  const createdCategory = await categoryService.createCategory(category);
-  res.status(200).send(createdCategory);
+  try {
+    const { category } = req.params;
+    const createdCategory = await categoryService.createCategory(category);
+    return res.status(200).json(createdCategory);
+  } catch (error) {
+    console.log(error.data);
+    return res.status(404).json({ error: error.message, message: 'Category doesnt exist' });
+  }
 };
 
 const getFeatures = async (req, res) => {
-  const {
-    category,
-  } = req.query;
-  const features = await categoryService.getFeatures(category);
-  res.status(200).send(features);
+  try {
+    const {
+      category,
+    } = req.query;
+    const features = await categoryService.getFeatures(category);
+    return res.status(200).json(features);
+  } catch (error) {
+    console.log(error.data);
+    return res.status(404).json({ message: 'Category doesnt exist' });
+  }
 };
 
 const getItems = async (req, res) => {
@@ -19,7 +29,7 @@ const getItems = async (req, res) => {
     category, color, size, brand,
   } = req.query;
   const features = await categoryService.getItems(category, color, size, brand);
-  res.status(200).send(features);
+  res.status(200).json(features);
 };
 
 module.exports = { createCategory, getFeatures, getItems };
--- src/services/category.service.js
+++ src/services/category.service.js
@@ -3,63 +3,75 @@ const axios = require('axios').default;
 const { Category, Item } = require('../models');
 
 const createCategory = async (category) => {
-  const categoryResponse = await axios.get(`https://backend-evaluation-lgsvu.ondigitalocean.app/category?name=${category}`);
+  try {
+    const categoryResponse = await axios.get(`https://backend-evaluation-lgsvu.ondigitalocean.app/category?name=${category}`);
 
-  const itemIdList = [];
+    const itemIdList = [];
 
-  const cateogoryItemsData = categoryResponse.data.itemMetadata.map(async (item) => {
-    itemIdList.push(item.id);
-    const itemFeatures = await axios.get(`https://backend-evaluation-lgsvu.ondigitalocean.app/items/${item.id}`);
-    return itemFeatures.data;
-  });
-
-  const resolvedItemData = await Promise.all(cateogoryItemsData);
-
-  for (let index = 0; index < resolvedItemData.length; index += 1) {
-    await Item.create({
-      itemId: itemIdList[index],
-      description: categoryResponse.data.itemMetadata[index].description,
-      color: resolvedItemData[index].features[0].value,
-      size: resolvedItemData[index].features[1].value,
-      brand: resolvedItemData[index].features[2].value,
-      name: categoryResponse.data.itemMetadata[index].name,
-      category,
+    const cateogoryItemsData = categoryResponse.data.itemMetadata.map(async (item) => {
+      itemIdList.push(item.id);
+      const itemFeatures = await axios.get(`https://backend-evaluation-lgsvu.ondigitalocean.app/items/${item.id}`);
+      return itemFeatures.data;
     });
-  }
 
-  const createdCategory = await Category.create({
-    name: categoryResponse.data.name,
-    description: categoryResponse.data.description,
-    items: itemIdList,
-  });
-  return createdCategory.dataValues;
+    const resolvedItemData = await Promise.all(cateogoryItemsData);
+
+    for (let index = 0; index < resolvedItemData.length; index += 1) {
+      await Item.create({
+        itemId: itemIdList[index],
+        description: categoryResponse.data.itemMetadata[index].description,
+        color: resolvedItemData[index].features[0].value,
+        size: resolvedItemData[index].features[1].value,
+        brand: resolvedItemData[index].features[2].value,
+        name: categoryResponse.data.itemMetadata[index].name,
+        category,
+      });
+    }
+
+    const createdCategory = await Category.create({
+      name: categoryResponse.data.name,
+      description: categoryResponse.data.description,
+      items: itemIdList,
+    });
+    return createdCategory.dataValues;
+  } catch (error) {
+    console.log(error);
+    throw (error);
+  }
 };
 
 const getFeatures = async (category) => {
-  const items = await Item.findAll({ where: { category } });
-  const output = {};
-  output.features = {};
-  output.features.color = [];
-  output.features.size = [];
-  output.features.brand = [];
-  items.forEach((item) => {
-    output.features.color.push(item.dataValues.color);
-    output.features.size.push(item.dataValues.size);
-    output.features.brand.push(item.dataValues.brand);
-  });
-  console.log(output);
-  return output;
+  try {
+    const items = await Item.findAll({ where: { category } });
+    const output = {};
+    output.features = {};
+    output.features.color = [];
+    output.features.size = [];
+    output.features.brand = [];
+    items.forEach((item) => {
+      output.features.color.push(item.dataValues.color);
+      output.features.size.push(item.dataValues.size);
+      output.features.brand.push(item.dataValues.brand);
+    });
+    return output;
+  } catch (error) {
+    console.log(error);
+    throw (error);
+  }
 };
 
 const getItems = async (category, color, size, brand) => {
+  let items;
   if (category !== undefined && size !== undefined && color !== undefined && brand !== undefined) {
-    const items = await Item.findAll({
+    items = await Item.findAll({
       where: {
-        category: '*', size, color, brand,
+        category, size, color, brand,
       },
     });
-    console.log(items);
+  } else {
+    items = 'This only valid when all the features are given';
   }
+  return items;
 };
 
 module.exports = { createCategory, getFeatures, getItems };
