d8422c91e5dd923a65725406f2fd0fbdeea4dfe4
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-26 13:19:54 +0530

    fix(route): fix post route

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 /* eslint-disable quotes */
 const express = require("express");
-const { healthRouter } = require("./src/routes");
+const { healthRouter, categoryRouter } = require("./src/routes");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  console.log(req.query);
   res.send("Welcome");
 });
 
+app.use(express.json());
+
 app.use("/health", healthRouter);
+app.use("/category", categoryRouter);
 
 app.listen(3000, () => {
   console.log(`Server is up at ${port}`);
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -3,7 +3,7 @@ const categoryHandler = require('../handlers/category.handler');
 
 const categoryRouter = express.Router();
 
-categoryRouter.put('/:category', categoryHandler.createCategory);
+categoryRouter.post('/:category', categoryHandler.createCategory);
 categoryRouter.get('/features', categoryHandler.getFeatures);
 categoryRouter.get('/', categoryHandler.getItems);
 module.exports = {
